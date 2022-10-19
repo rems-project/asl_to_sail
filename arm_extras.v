b62845705d864f74b9e161b43d3470947b364a52
Brian Campbell <Brian.Campbell@ed.ac.uk>
2022-10-19 14:51:32 +0100

    Add a monadic placeholder for print_endline for Coq OCaml extraction

--- arm_extras.v
+++ arm_extras.v
@@ -10,6 +10,10 @@ Definition write_ram {rv e} addrsize size (hexRAM : mword addrsize) (address : m
 Definition read_ram {rv e} addrsize size `{ArithFact (size >=? 0)} (hexRAM : mword addrsize) (address : mword addrsize) : monad rv (mword (8 * size)) e :=
   read_mem Read_plain addrsize address size.
 
+(* A version of print_endline in the monad so that it doesn't disappear during
+   extraction to OCaml.  A change in the Sail library declaration has to be
+   spliced in to use it. *)
+Definition print_endline_monadic {rv e} (_:string) : monad rv unit e := returnm tt.
 
 (* Arithmetic lemmas that have been useful in previous specs *)
 
